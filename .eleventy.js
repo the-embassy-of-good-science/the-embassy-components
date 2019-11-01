3fbecb7510f21226d1e023fe3e0a003714af158b
Ruben Nascimento <ruben.nascimento@bzin.net>
2019-11-01 13:47:26 +0100

    Add syntax with Prism and eleventy plugin

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,28 @@
-module.exports = {
-    dir: {
-        input: "src",
-        output: "docs"
-    }
+const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
+const Terser = require("terser");
+
+module.exports = function(eleventyConfig) {
+    eleventyConfig.addPlugin(syntaxHighlight);
+
+    // Copy static folder to "_site/static"
+    eleventyConfig.addPassthroughCopy({ "src/static": "static" });
+
+    // Minify javascript
+    eleventyConfig.addFilter("jsmin", function(code) {
+        let minified = Terser.minify(code);
+        if( minified.error ) {
+            console.log("Terser error: ", minified.error);
+            return code;
+        }
+
+        return minified.code;
+    });
+
+    return {
+        dir: {
+            input: "src",
+            output: "docs"
+        },
+        htmlTemplateEngine: "njk"
+    };
 };

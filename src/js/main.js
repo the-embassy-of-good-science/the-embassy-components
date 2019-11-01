ab6c6e9500e6b08919a2cbdc932ad1d0078baeba
Ruben Nascimento <ruben.nascimento@bzin.net>
2019-11-01 14:43:01 +0100

    Add webpack configuration to repo

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,5 @@
+// SCSS files from bootstrap
+import '../scss/main.scss';
+
+// Import bootstrap logic
 import 'bootstrap';
---
+++ webpack.config.js
@@ -0,0 +1,152 @@
+// Loaded modules
+const path = require('path')
+const ManifestPlugin = require('webpack-manifest-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin')
+const ErrorOverlayPlugin = require('error-overlay-webpack-plugin')
+const TerserPlugin = require('terser-webpack-plugin')
+
+// Project folder
+const outputFolder = './src/static/'
+
+module.exports = (env, argv) => {
+  const devMode = argv.mode === 'development'
+
+  const webpackConfig = {
+    entry: {
+      'main': [ './src/js/main.js' ],
+    },
+    output: {
+      path: path.resolve(__dirname, outputFolder),
+      filename: 'js/[name].js',
+      chunkFilename: 'js/[name].js',
+      // Easier navigation in Chrome dev tool source mapping:
+      devtoolModuleFilenameTemplate: info => path.resolve(info.absoluteResourcePath).replace(/\\/g, '/')
+    },
+    resolve: {
+      modules: [
+        'node_modules',
+      ]
+    },
+    module: {
+      rules: [
+        {
+          enforce: 'pre',
+          test: /\.js$/,
+          exclude: /node_modules/,
+          loader: 'eslint-loader',
+        },
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          loader: 'babel-loader',
+        },
+        {
+          test: /\.(sa|sc|c)ss$/,
+          use: [
+            {
+              loader: MiniCssExtractPlugin.loader,
+            },
+            {
+              loader: 'css-loader',
+              options: {
+                sourceMap: true
+              }
+            }, {
+              loader: 'postcss-loader',
+              options: {
+                sourceMap: true
+              }
+            }, {
+              loader: 'sass-loader',
+              options: {
+                sourceMap: true
+              }
+            }
+
+          ],
+        },
+      ]
+    },
+    // https://webpack.js.org/configuration/performance/
+    performance: {
+      maxAssetSize: 250000,
+      // maxEntrypointSize: 300000,
+      hints: 'warning',
+      assetFilter: function (assetFilename) {
+        if (devMode) return // only run the filesize check when in production mode
+        return !(/\.map$/.test(assetFilename)) // ignore .map files
+      }
+    },
+    optimization: {
+      minimizer: [
+        new OptimizeCSSAssetsPlugin({})
+      ],
+      splitChunks: {
+        cacheGroups: {
+          'vendors': {
+            test: /node_modules/,
+            chunks: (chunk) => {
+              return chunk.name === 'main'
+            },
+            name: 'vendors',
+            enforce: true,
+          }
+        },
+      }
+    },
+    plugins: [
+      // new CleanWebpackPlugin([
+      //   // wipe the entire static folder
+      //   path.resolve(__dirname, 'static/js'),
+      //   path.resolve(__dirname, 'static/css'),
+      // ], {
+      //   // except of this file
+      //   exclude: ['prism.css']
+      // }),
+      new CleanWebpackPlugin({
+        cleanOnceBeforeBuildPatterns: [
+          path.resolve(__dirname, 'static/js'),
+          path.resolve(__dirname, 'static/css'),
+          '!prism.css'
+        ]
+      }),
+      new ManifestPlugin({
+        publicPath: '/static/',
+        fileName: 'manifest.json'
+      }),
+      new MiniCssExtractPlugin({
+        publicPath: '/static/',
+        filename: 'css/[name].css',
+      }),
+      new ErrorOverlayPlugin()
+    ]
+  }
+
+  if (devMode) {
+    webpackConfig.optimization.minimizer.push(new TerserPlugin({
+      sourceMap: true,
+      cache: true,
+      parallel: true,
+    }))
+  }
+
+  if (!devMode) {
+    webpackConfig.optimization.minimizer.push(new TerserPlugin({
+      sourceMap: true,
+      extractComments: true,
+      cache: true,
+      parallel: true,
+      terserOptions: {
+        mangle: {
+          safari10: true,
+          keep_classnames: true,
+          keep_fnames: true,
+        }
+      }
+    }))
+  }
+
+  return webpackConfig
+}

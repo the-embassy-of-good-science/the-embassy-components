a22a21fa37fb93875ff91a5cf1471d773c6c1248
Ruben Nascimento <ruben.nascimento@bzin.net>
2019-11-19 14:57:44 +0100

    new assets files

--- src/js/library.js
+++ src/js/library.js
@@ -1,1 +1,17 @@
+// jQuery is needed by Bootstrap
+import $ from 'jquery';
+
 import '../scss/components-library.scss';
+
+// Import all bootstrap components
+import 'bootstrap';
+
+// Initialize added components
+// eslint-disable-next-line func-names
+$(function() {
+  // load popover
+  $('[data-toggle="popover"]').popover();
+
+  // load tooltip
+  $('[data-toggle="tooltip"]').tooltip();
+});
--- src/js/main.js
+++ src/js/main.js
@@ -1,35 +1,8 @@
 // jQuery is needed by Bootstrap
-import $ from 'jquery';
+// import $ from 'jquery';
 
 // SCSS files from bootstrap
 import '../scss/main.scss';
 
 // Import svg
 import '../svgs/index';
-
-// Import all bootstrap components
-// import 'bootstrap';
-
-// Import bootstrap components individualy
-// import 'bootstrap/js/src/alert';
-import 'bootstrap/js/src/button';
-// import 'bootstrap/js/src/carousel';
-// import 'bootstrap/js/src/collapse';
-// import 'bootstrap/js/src/dropdown';
-// import 'bootstrap/js/src/modal';
-import 'bootstrap/js/src/popover';
-// import 'bootstrap/js/src/scrollspy';
-// import 'bootstrap/js/src/tab';
-// import 'bootstrap/js/src/toast';
-import 'bootstrap/js/src/tooltip';
-// import 'bootstrap/js/src/util';
-
-// Initialize added components
-// eslint-disable-next-line func-names
-$(function() {
-  // load popover
-  $('[data-toggle="popover"]').popover();
-
-  // load tooltip
-  $('[data-toggle="tooltip"]').tooltip();
-});

f7ff447aea534fe5f1d687f7354645d2d72adea1
Ruben Nascimento <ruben.nascimento@bzin.net>
2019-11-26 17:57:15 +0100

    Add new columns

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,25 @@
 // jQuery is needed by Bootstrap
 // import $ from 'jquery';
+import Macy from 'macy';
 
 // SCSS files from bootstrap
 import '../scss/main.scss';
+
+// eslint-disable-next-line no-unused-vars
+const macyInstance = Macy({
+  // for all available options: https://github.com/bigbite/macy.js
+  container: '#columns-container',
+  columns: 3,
+  margin: {
+    x: 30,
+    y: 30,
+  },
+  breakAt: {
+    600: {
+      columns: 1,
+    },
+    960: {
+      columns: 2,
+    },
+  },
+});

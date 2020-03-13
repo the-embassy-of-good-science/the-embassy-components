cab2d556821cd9da6cb0842821e83bcc4dc5081b
Ruben Nascimento <ruben.nascimento@bzin.net>
2020-03-13 13:04:03 +0100

    try small fix for undefined issue in navigation on SMW compilation

--- src/js/navigation.js
+++ src/js/navigation.js
@@ -2,22 +2,16 @@
  * Navigation module
  */
 
-var navigation = document.querySelector('.navigation');
-var mobileTrigger = document.querySelector('.nav-trigger');
-var navigationCheckbox = document.querySelector('#nav-trigger-home');
+document.querySelector('#nav-trigger-home').addEventListener('click', function(){
+  document.querySelector('.navigation').classList.toggle('is-open');
+  document.body.style.overflow = document.querySelector('.navigation').classList.contains('is-open') ? 'hidden' : '';
+});
 
-if(mobileTrigger) {
-  mobileTrigger.addEventListener('click', function(){
-    navigation.classList.toggle('is-open')
-    document.body.style.overflow = navigation.classList.contains('is-open') ? 'hidden' : '';
-  });
-
-  window.addEventListener('resize', function(e){
-    if(window.innerWidth >= 1024 && navigation.classList.contains('is-open')) {
-      navigation.classList.remove('is-open');
-      document.body.style.overflow = ''
-      navigationCheckbox.checked = false
-    }
-  });
-}
+window.addEventListener('resize', function(e){
+  if(window.innerWidth >= 1024 && document.querySelector('.navigation').classList.contains('is-open')) {
+    navigation.classList.remove('is-open');
+    document.body.style.overflow = ''
+    navigationCheckbox.checked = false
+  }
+});
 

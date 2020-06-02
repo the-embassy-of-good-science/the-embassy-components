ffcf22201c1a1b3df7dd6d1f929f49618924ff35
Ruben Nascimento <ruben.nascimento@bzin.net>
2020-06-02 16:27:53 +0200

    Add pollyfil for remove

--- src/js/drilldown-cleanup.js
+++ src/js/drilldown-cleanup.js
@@ -1,4 +1,27 @@
 /**
+ * Polyfill for ChildNode.remove()
+ * More: https://developer.mozilla.org/en-US/docs/Web/API/ChildNode/remove
+ */
+(function (arr) {
+  arr.forEach(function (item) {
+    if (item.hasOwnProperty('remove')) {
+      return;
+    }
+    Object.defineProperty(item, 'remove', {
+      configurable: true,
+      enumerable: true,
+      writable: true,
+      value: function remove() {
+        if (this.parentNode === null) {
+          return;
+        }
+        this.parentNode.removeChild(this);
+      }
+    });
+  });
+})([Element.prototype, CharacterData.prototype, DocumentType.prototype]);
+
+/**
  * After Drilldown is loaded this javascript will do some cleaning
  * on the DOM.
  */

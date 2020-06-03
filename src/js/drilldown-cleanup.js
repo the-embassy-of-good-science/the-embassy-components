dea056dbe1683e1a114377a07fa8fb702c3904b7
Ruben Nascimento <ruben.nascimento@bzin.net>
2020-06-03 12:48:44 +0200

    Refactor cleanup on filters

--- src/js/drilldown-cleanup.js
+++ src/js/drilldown-cleanup.js
@@ -29,11 +29,39 @@
 var drilldownHead = document.querySelector('#drilldown-header')
 
 // DOM drilldown form filters
-var drilldownFilters = document.querySelector('#drilldown-filters')
+var drilldownFilters = document.querySelector('div.drilldown-filters')
+var drilldownForms = drilldownFilters.querySelectorAll('form')
 
-// We use a setTimeout to wait for the DOM to be loaded
-setTimeout(function(){
-  //console.log(`Drilldown Cleanup`)
+/**
+ * This method created some simple funcionality on current drilldown forms
+ *
+ * @param element
+ * @constructor
+ */
+var FormDrilldown = function(element) {
+
+  if(!element) return
+
+  // get DOM elements from forms
+  var el = element
+  var submit = el.querySelector('input[type="submit"]')
+  var select = el.querySelector('select')
+
+  // @todo to disable in server
+  select.removeAttribute('style')
+
+  // detect change on select element
+  select.addEventListener('change', function() {
+    // trigger submit button
+    submit.click()
+  })
+}
+
+/**
+ * This method cleans unnecessary code on the selected filters
+ *
+ */
+var cleanupSelectedFilters = function() {
   var selectedTags =  drilldownHead.querySelectorAll('.drilldown-header-value')
 
   // We loop over the selected filters to clean these
@@ -43,12 +71,35 @@ setTimeout(function(){
     if (selectedTags[i].innerText === '&') {
       selectedTags[i].remove()
 
-    // if the filter is a selection we remove the first 3 characters and the last one so that
-    // we can see the correct selection
+      // if the filter is a selection we remove the first 3 characters and the last one so that
+      // we can see the correct selection
     } else {
-      selectedTags[i].innerText = selectedTags[i].innerText.substr(3)
-      selectedTags[i].innerText = selectedTags[i].innerText.substr(0, selectedTags[i].innerText.length - 1)
+
+      // Remove ' or " from the end of selected filter values
+      if(selectedTags[i].innerText[selectedTags[i].innerText.length - 1] === ("'" || '"')) {
+        selectedTags[i].innerText = selectedTags[i].innerText.substr(0, selectedTags[i].innerText.length - 1)
+      }
+
+      // remove `~ '` from string
+      // @todo not sure why the string was being added in the name of the filter
+      if(selectedTags[i].innerText.substr(0, 3) === "~ '") {
+        selectedTags[i].innerText = selectedTags[i].innerText.substr(3)
+      }
     }
+  }
+}
+
+// We use a setTimeout to wait for the DOM to be loaded
+setTimeout(function(){
+
+  // On load we do a clean up on selected filters
+  // The purpose is to remove inconsistences from SMW / Drilldown Filters
+  cleanupSelectedFilters()
+
+  // Detect change on select dropdown
+  for(i = 0; i < drilldownForms.length; i++) {
+    new FormDrilldown(drilldownForms[i])
   }
 
 }, 0)
+

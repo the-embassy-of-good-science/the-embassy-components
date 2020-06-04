325a73ac9d650ed83350e5c84f5a6a1ccc64c4ae
Ruben Nascimento <ruben.nascimento@bzin.net>
2020-06-04 17:31:31 +0200

    navigation error fix

--- src/js/navigation.js
+++ src/js/navigation.js
@@ -2,13 +2,18 @@
  * Navigation module
  */
 
-document.querySelector('#nav-trigger-home').addEventListener('click', function(){
-  document.querySelector('.navigation').classList.toggle('is-open');
-  document.body.style.overflow = document.querySelector('.navigation').classList.contains('is-open') ? 'hidden' : '';
-});
+if(document.querySelector('#nav-trigger-home')) {
+  document.querySelector('#nav-trigger-home').addEventListener('click', function(){
+    document.querySelector('.navigation').classList.toggle('is-open');
+    document.body.style.overflow = document.querySelector('.navigation').classList.contains('is-open') ? 'hidden' : '';
+  });
+}
+
 
 window.addEventListener('resize', function(e){
-  if(window.innerWidth >= 1024 && document.querySelector('.navigation').classList.contains('is-open')) {
+  var navigation = document.querySelector('.navigation')
+  var navigationCheckbox = document.querySelector('#nav-trigger-home')
+  if(window.innerWidth >= 1024 && navigation.classList.contains('is-open')) {
     navigation.classList.remove('is-open');
     document.body.style.overflow = ''
     navigationCheckbox.checked = false

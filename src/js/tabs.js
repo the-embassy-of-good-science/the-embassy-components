10fd68bb357ca923c57fcb487144a4a9990c9c5b
Ruben Nascimento <ruben.nascimento@bzin.net>
2020-10-23 12:15:38 +0100

    Typo on tabs.js

--- src/js/tabs.js
+++ src/js/tabs.js
@@ -1,38 +1,37 @@
 import $ from 'jquery';
 
-$(function () {
-  const traineeTab = document.querySelector('[data-tab="trainee"')
+$(function() {
+  const traineeTab = document.querySelector('[data-tab="trainee"]');
 
   // Trainee tab active by default
   if (traineeTab) {
-    traineeTab.classList.add('active')
+    traineeTab.classList.add('active');
   }
 
-})
+});
 
 /**
  * Module to open links inside MediaWiki context
  */
-$('.g-tab').on('click', function (e) {
+$('.g-tab').on('click', function(e) {
   e.preventDefault();
 
-  const trainerTab = document.querySelector('[data-tab="trainer"')
-  const traineeTab = document.querySelector('[data-tab="trainee"')
+  const trainerTab = document.querySelector('[data-tab="trainer"]');
+  const traineeTab = document.querySelector('[data-tab="trainee"]');
 
   if (e.target.dataset.tab === 'trainer') {
-    trainerTab.classList.add('active')
-    traineeTab.classList.remove('active')
+    trainerTab.classList.add('active');
+    traineeTab.classList.remove('active');
     document.querySelector('#data-tab-content-trainer').style.display = 'block';
     document.querySelector('#data-tab-content-trainee').style.display = 'none';
 
   }
 
   if (e.target.dataset.tab === 'trainee') {
-    traineeTab.classList.add('active')
-    trainerTab.classList.remove('active')
+    traineeTab.classList.add('active');
+    trainerTab.classList.remove('active');
     document.querySelector('#data-tab-content-trainer').style.display = 'none';
     document.querySelector('#data-tab-content-trainee').style.display = 'block';
   }
-
 
 });

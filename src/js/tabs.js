0634d8408983dfa60d8efa12dabcf0e29297af49
Ruben Nascimento <ruben.nascimento@bzin.net>
2020-10-29 12:22:27 +0000

    fix on tabs

--- src/js/tabs.js
+++ src/js/tabs.js
@@ -4,7 +4,7 @@
 document.addEventListener('DOMContentLoaded', (event) => {
   var tabs = document.querySelectorAll('.g-tablinks:not(.disabled)')
 
-  if(tabs) {
+  if(tabs.length) {
     // Set first tab that is enabled
     tabs[0].classList.add('active')
   }
@@ -14,24 +14,26 @@ document.addEventListener('DOMContentLoaded', (event) => {
  * Module to open links inside MediaWiki context
  */
 var tabs = document.querySelectorAll('.g-tablinks')
-tabs.forEach(function(tab) {
+if(tabs) {
+  tabs.forEach(function(tab) {
 
-  tab.addEventListener('click', function(e) {
-    var trainerTab = document.querySelector('[data-tab="trainer"]')
-    var traineeTab = document.querySelector('[data-tab="trainee"]')
-
-    if (e.target.dataset.tab === 'trainer') {
-      trainerTab.classList.add('active')
-      traineeTab.classList.remove('active')
-      document.querySelector('#data-tab-content-trainer').style.display = 'block';
-      document.querySelector('#data-tab-content-trainee').style.display = 'none';
-    }
-
-    if (e.target.dataset.tab === 'trainee') {
-      traineeTab.classList.add('active')
-      trainerTab.classList.remove('active')
-      document.querySelector('#data-tab-content-trainer').style.display = 'none';
-      document.querySelector('#data-tab-content-trainee').style.display = 'block';
-    }
+    tab.addEventListener('click', function(e) {
+      var trainerTab = document.querySelector('[data-tab="trainer"]')
+      var traineeTab = document.querySelector('[data-tab="trainee"]')
+  
+      if (e.target.dataset.tab === 'trainer') {
+        trainerTab.classList.add('active')
+        traineeTab.classList.remove('active')
+        document.querySelector('#data-tab-content-trainer').style.display = 'block';
+        document.querySelector('#data-tab-content-trainee').style.display = 'none';
+      }
+  
+      if (e.target.dataset.tab === 'trainee') {
+        traineeTab.classList.add('active')
+        trainerTab.classList.remove('active')
+        document.querySelector('#data-tab-content-trainer').style.display = 'none';
+        document.querySelector('#data-tab-content-trainee').style.display = 'block';
+      }
+    })
   })
-})
+}
